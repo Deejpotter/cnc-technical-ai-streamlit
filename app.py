97a2b27ff5ed74fd5863fe1ef77e5a4ac534554e
Deej Potter <deejpotter@gmail.com>
2023-09-07 22:29:36 +1000

    Enhance Streamlit chat UI with comments and input clearing

--- app.py
+++ app.py
@@ -1,42 +1,51 @@
 import streamlit as st
-
-# Create an instance of the ChatEngine class
-# This will run the __init__ method in the ChatEngine class
 from chat_engine import ChatEngine
 
+# Initialize the ChatEngine
 chat_engine = ChatEngine()
 
 
-# Function to manage chat state
+# Function to manage chat state using Streamlit's cache
 @st.cache_resource
 def manage_chat_state():
+    # Initialize an empty chat state
     return {"chat": []}
 
 
-# Sidebar for conversations
+# Sidebar for listing conversations
 st.sidebar.title("Conversations")
 conversations = ["Conversation 1", "Conversation 2"]
+# Dropdown to select a conversation
 selected_conversation = st.sidebar.selectbox("Select a conversation", conversations)
 
 # Main chat area
 st.title(f"Chat - {selected_conversation}")
 
-# Get the chat state
+# Retrieve the current chat state
 chat_state = manage_chat_state()
 
-# Text input for user message
-user_message = st.text_input("Type your message here...")
+# Create a placeholder for the text input
+text_input_placeholder = st.empty()
 
-# Send button
+# Text input for user message
+# We use the placeholder to create the text input so that we can clear it later
+user_message = text_input_placeholder.text_input("Type your message here...")
+
+# Send button to submit the message
 if st.button("Send"):
-    # Process the user message through your ChatEngine
+    # Process the user's message and get the bot's response
     bot_response = chat_engine.process_user_input(user_message)
 
-    # Update chat state
+    # Update the chat state with the new messages
     chat_state["chat"].append({"user": user_message, "bot": bot_response})
 
-# Display chat history
-st.write("Chat History:")
+    # Clear the text input field by updating the placeholder
+    text_input_placeholder.text_input("Type your message here...", value="", key="new_key_for_input")
+
+# Display the chat history
+st.write("## Chat History:")
 for chat in chat_state["chat"]:
-    st.write(f"User: {chat['user']}")
-    st.write(f"Bot: {chat['bot']}")
+    # Display user's messages
+    st.markdown(f"**You**: {chat['user']}")
+    # Display bot's messages
+    st.markdown(f"_Assistant_: {chat['bot']}")
